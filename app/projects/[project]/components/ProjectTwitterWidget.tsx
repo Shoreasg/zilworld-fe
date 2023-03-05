4f31c7f8b6e0c5c9cc5993a5b6c9b66f9d2c7eae
Gan Wei Lun <ganweilun@Gans-MacBook-Pro-2.local>
2023-03-05 15:27:58 +0800

    Fix if twitter is not avaliable, remove is loading

--- app/projects/[project]/components/ProjectTwitterWidget.tsx
+++ app/projects/[project]/components/ProjectTwitterWidget.tsx
@@ -8,30 +8,36 @@ export default function ProjectTwitterWidget({
   projectData,
 }: ProjectDetailsCardProps) {
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  console.log(isLoading);
   return (
     <div className="flex flex-1 flex-col md:pl-64">
       <div className="p-6">
         <h3 className="text-2xl font-semibold text-gray-900 mb-6">
           Twitter Updates
         </h3>
-        {isLoading && projectData.twitter && (
+        {isLoading && isError === false && projectData.twitter && (
           <div className="px-4 py-5 sm:p-6 font-semibold opacity-80 text-gray-900 bg-gray-500 text-center animate-pulse ">
             Loading
           </div>
         )}
-        <Timeline
-          dataSource={{
-            sourceType: "profile",
-            screenName: `${projectData.twitter}`,
-          }}
-          options={{ height: "650" }}
-          onLoad={() => setIsLoading(false)}
-          renderError={(_err) => (
-            <div className="px-4 py-5 sm:p-6 font-semibold opacity-80 text-gray-900 bg-gray-400 text-center">
-              Twitter not avaliable
-            </div>
-          )}
-        />
+        {isError === true ? (
+          <div className="px-4 py-5 sm:p-6 font-semibold opacity-80 text-gray-900 bg-gray-400 text-center">
+            Twitter not avaliable
+          </div>
+        ) : (
+          <Timeline
+            dataSource={{
+              sourceType: "profile",
+              screenName: `${projectData.twitter}`,
+            }}
+            options={{ height: "650" }}
+            onLoad={() => setIsLoading(false)}
+            renderError={(_err) => (
+              <>{_err ? setIsError(true) : setIsError(false)}</>
+            )}
+          />
+        )}
       </div>
     </div>
   );
