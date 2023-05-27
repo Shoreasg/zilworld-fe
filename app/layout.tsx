c7be7c668b8e1677c7500a31ff103cc0ce3450e3
Wei Lun <garyganweilun@gmail.com>
2023-05-28 03:00:33 +0800

    Navbar layout done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { AnalyticsWrapper } from "./analytics";
 import NavBar from "./components/NavBar";
+import NavBarV2 from "./components/NavBarV2";
 import "./globals.css";
 import {Plus_Jakarta_Sans, Lato} from "@next/font/google";
 
@@ -24,10 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${plusJakartaSans.variable} ${lato.variable}`}>
       <head />
-      <body className="h-full bg-gray-300">
-        <main className="h-full">
-          <NavBar />
-          {children}
+      <body>
+        <main className="bg-[#f5f5f5] h-screen w-screen flex">
+          <NavBarV2 />
+          {/* {children} */}
           <AnalyticsWrapper />
         </main>
       </body>
