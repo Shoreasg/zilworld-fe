76912d5e48d7d666650f0810accd478038269da1
Wei Lun <garyganweilun@gmail.com>
2023-09-09 23:45:00 +0800

    Remove token code

--- app/tokens/page.tsx
+++ app/tokens/page.tsx
@@ -29,7 +29,6 @@ export default function Home() {
         </div>
         <div className="py-6">
           <div className="mx-auto py-6 max-w-7xl px-4 sm:px-6 md:px-8 flex justify-center">
-            {/* @ts-expect-error Server Component */}
             <TokenCards />
           </div>
         </div>
